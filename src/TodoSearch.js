bcb4f2072047df38059642c79f84032581354519
Pablo Aristizabal <radabeca23@gmail.com>
2024-10-09 14:47:58 -0500

    hook useState

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './TodoSearch.css'
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
+    console.log('Todos de: ' + searchValue);
     return (
-        <input placegolder="Cortar cebolla" className="TodoSearch" onChange={(event) => {
-            console.log(event.target.value)
+        <input placeholder="search ToDo" className="TodoSearch" value={searchValue} onChange={(event) => {
+            setSearchValue(event.target.value);
         }
         }></input>
     )
