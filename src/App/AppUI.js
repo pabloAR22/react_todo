3d0776e3fbd1cee49459ed42c306332263030d7b
Pablo Aristizabal <radabeca23@gmail.com>
2024-11-09 11:53:07 -0500

    Loading Skeletons

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -3,6 +3,10 @@ import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
+import { TodosLoading } from '../TodosLoading';
+import { TodosError } from '../TodosError';
+import { EmptyTodos } from '../EmptyTodos';
+
 
 function AppUI ({
     loading, 
@@ -24,9 +28,9 @@ function AppUI ({
             setSearchValue = {setSearchValue}
           />
           <TodoList>
-            {loading && <p>Estamos cargando...</p>}
-            {error && <p>Hubo un error</p>}
-            {(!loading && searchedTodos.length === 0) && <p>Crea tu primer Todo</p>}
+            {loading && <TodosLoading />}
+            {error && <TodosError />}
+            {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
             
             { searchedTodos.map(todo => (
               <TodoItem
---
+++ src/EmptyTodos/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import './EmptyTodos.css'
+
+function EmptyTodos() {
+    return (
+        <p>No hay todos, crea uno..</p>
+    )
+}
+
+export { EmptyTodos };
