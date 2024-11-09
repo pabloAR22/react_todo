c27eb88a6d051ef4717b7b293d1d8ea80de31e46
Pablo Aristizabal <radabeca23@gmail.com>
2024-11-09 10:31:57 -0500

    rework

--- src/App/UseLocalStorage.js
+++ src/App/UseLocalStorage.js
@@ -1,17 +1,32 @@
 import React from "react";
 
 function useLocalStorage(itemName, initialValue) {
-    const localStorageItem = localStorage.getItem(itemName);
-    let parsedItem;
 
-    if (!localStorageItem) {
-        localStorage.setItem(itemName, JSON.stringify(initialValue));
-        parsedItem = [];
-    } else {
-        parsedItem = JSON.parse(localStorageItem);
-    }
+    const [item, setItem] = React.useState(initialValue);
+    const [loading, setLoading] = React.useState(true);
+    const [error, setError] = React.useState(false);
 
-    const [item, setItem] = React.useState(parsedItem);
+    React.useEffect( () => {
+        setTimeout( () => {
+            try {
+                const localStorageItem = localStorage.getItem(itemName);
+                let parsedItem;
+                
+                if (!localStorageItem) {
+                    localStorage.setItem(itemName, JSON.stringify(initialValue));
+                    parsedItem = [];
+                } else {
+                    parsedItem = JSON.parse(localStorageItem);
+                    setItem(parsedItem);
+                }
+        
+                setLoading(false);
+            } catch (error) {
+                setLoading(false);
+                setError(error); 
+            }
+        }, 2000);
+    }, []);
 
     const saveItem = (newItem) => {
         localStorage.setItem(itemName, JSON.stringify(newItem));
@@ -19,7 +34,12 @@ function useLocalStorage(itemName, initialValue) {
         setItem(newItem);
     };
 
-    return [item, saveItem];
+    return {
+        item,
+        saveItem,
+        loading,
+        error
+    };
 }
 
 export { useLocalStorage };
