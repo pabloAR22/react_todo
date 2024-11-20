66e35aa8b7236e96d2eca0c8280bee54d49fe652
Pablo Aristizabal <radabeca23@gmail.com>
2024-11-20 18:26:19 -0500

    Modal

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -42,7 +42,9 @@ function AppUI () {
                 />
               ))}
             </TodoList>
-          <CreateTodoButton />
+          <CreateTodoButton 
+            setOpenModal={setOpenModal}
+          />
 
           {openModal && (
             <Modal>
--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,11 +1,12 @@
 import './CreateTodoButton.css';
 
-function CreateTodoButton() {
+function CreateTodoButton( {setOpenModal} ) {
     return (
-        <button className="CreateTodoButton" onClick={(event) => {
-            console.log(event);
-            console.log(event.target)
-        }} >+</button>
+        <button className="CreateTodoButton" onClick={
+            () => {
+                setOpenModal(state => !state);
+            }
+        } >+</button>
     )
 }
 
